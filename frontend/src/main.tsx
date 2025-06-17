fa3ae51035d791856388729a3fb5ad4c8dbbee72
Anurag Prajapati <prajapatianurag73240@gmail.com>
2025-06-17 08:19:16 +0530

    feat(store): wrap app with Redux Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes.tsx'
+import {Provider} from 'react-redux'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
     <RouterProvider router={routes}/>
+    </Provider>
   </StrictMode>,
 )
