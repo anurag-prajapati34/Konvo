d3d7d4682c7989805332aa6c2e0426b4b3d2649f
Anurag Prajapati <prajapatianurag73240@gmail.com>
2025-06-17 11:21:12 +0530

    feat (auth) : add reducer to set user credentials

--- frontend/src/redux/slices/authSlice.ts
+++ frontend/src/redux/slices/authSlice.ts
@@ -1,10 +1,25 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-const SERVER_URL=import.meta.env.VITE_SERVER_URL;
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
-const initialState = {
-  user: null,
-  loading: false,
+interface User {
+  userId:string,
+  name: string,
+  email: string,
+  imageUrl?:string,
+}
+interface initialStateInterface {
+  user: User ;
+  isLogedIn: boolean;
+}
+const initialState: initialStateInterface = {
+  user: {
+    userId: "",
+    name: "",
+    email: "",
+    imageUrl: "",
+  },
+  isLogedIn: false,
 };
 
 export const registerUser = createAsyncThunk(
@@ -19,13 +34,9 @@ export const registerUser = createAsyncThunk(
   ) => {
     try {
       await axios
-        .post(
-          `${SERVER_URL}/api/auth/user/register`,
-          credentials,
-          {
-            withCredentials: true,
-          }
-        )
+        .post(`${SERVER_URL}/api/auth/user/register`, credentials, {
+          withCredentials: true,
+        })
         .then((res) => {
           console.log("User Register Response", res);
         });
@@ -38,16 +49,12 @@ export const registerUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
   "auth/loginUser",
 
-  async (credentials :{email:string,password:string}, thunkAPI) => {
+  async (credentials: { email: string; password: string }, thunkAPI) => {
     try {
       await axios
-        .post(
-          `${SERVER_URL}/api/auth/user/login`,
-          credentials,
-          {
-            withCredentials: true,
-          }
-        )
+        .post(`${SERVER_URL}/api/auth/user/login`, credentials, {
+          withCredentials: true,
+        })
         .then((res) => {
           console.log("User login response", res);
         });
@@ -60,7 +67,16 @@ export const loginUser = createAsyncThunk(
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    setUserCredentials: (state, action) => {
+      const {name,email,_id}=action.payload;
+      state.user.userId=_id;
+      state.user.name=name;
+      state.user.email=email;
+      state.isLogedIn = true;
+    },
+  },
 });
 
+export const { setUserCredentials } = authSlice.actions;
 export default authSlice.reducer;
