1371daf45789aef1ee0ab4c09906f92f99a0d75a
Anurag Prajapati <prajapatianurag73240@gmail.com>
2025-06-22 11:19:52 +0530

    refactor (api) : updated user route path

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getUserProfile } = require("../controllers/user-controllers");
 const router = express.Router();
 
-router.get("/profile/other",getUserProfile );
+router.get("/profile",getUserProfile );
 
 module.exports = router;
