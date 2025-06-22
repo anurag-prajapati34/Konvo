b0a7e1702d6f7a1a2177e798d203325f15fbb812
Anurag Prajapati <prajapatianurag73240@gmail.com>
2025-06-22 10:02:12 +0530

    feat(model) : updated model to take more user info

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -14,6 +14,16 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    bio:{
+      type:string,
+      default:'Hey there i am using Konvo !'
+    },
+    imageUrl: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    },
+    
   },
   {
     timestamps: true,
